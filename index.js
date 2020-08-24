e8eb409f18a7844ef890f14b459a0f85030a2724
elizaglover <56939637+elizaglover@users.noreply.github.com>
2020-08-23 20:32:46 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+/**
+ * index.js
+ * Paul Krishnamurthy 2016
+ *
+ * https://paulkr.com
+ * paul@paulkr.com
+ */
+
+$(document).ready(function () {
+	$(".demo-img").blowup({
+		background : "#FCEBB6"
+	});
+})
+
+$(document).ready(function () {
+	$(".demo-img2").blowup({
+		background : "#FCEBB6"
+	});
+})
